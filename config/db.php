59222adc5ac0ec8f8f625dbdec4b2b8d11970ac4
zhushuangwen <zhushuangwen001@lianjia.com>
2018-08-13 15:49:25 +0800

    prod env change

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yiicms',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=business_cms',
+    'username' => 'zhushuangwen',
+    'password' => 'Wen@1275621518',
     'charset' => 'utf8',
 ];
--- web/index.php
+++ web/index.php
@@ -1,8 +1,9 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
+//defined('YII_DEBUG') or define('YII_DEBUG', true);//上线时，注释掉
+//defined('YII_ENV') or define('YII_ENV', 'test');//上线时，注释掉
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
